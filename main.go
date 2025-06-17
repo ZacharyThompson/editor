d99690550a7e31a22bcbeacba90f311367bd004d
Zachary Thompson <zachthompson406@gmail.com>
2025-06-16 19:39:18 -0700

    implement basic cursor

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	// "log"
-
+	"fmt"
 	rl "github.com/gen2brain/raylib-go/raylib"
+	// "log"
 )
 
 type Cursor struct {
@@ -21,17 +21,13 @@ func (c *Cursor) MoveRight(count, maxCol int) {
 	c.col = min(c.col+count, maxCol)
 }
 func (c *Cursor) MoveUp(count int) {
-	c.col = max(0, c.line-count)
+	c.line = max(0, c.line-count)
+}
+func (c *Cursor) MoveDown(count int, maxLine int) {
+	c.line = min(c.line+count, maxLine)
 }
 
 func main() {
-	font := rl.GetFontDefault()
-	// font := rl.LoadFontEx("./assets/Iosevka-Regular.ttf", 20, nil, 0)
-	// if !rl.IsFontValid(font) {
-	// 	log.Fatal("Invalid Font")
-	// }
-	defer rl.UnloadFont(font)
-
 	rl.SetConfigFlags(rl.FlagWindowResizable)
 	rl.InitWindow(800, 450, "editor")
 	defer rl.CloseWindow()
@@ -39,7 +35,6 @@ func main() {
 	rl.SetTargetFPS(120)
 
 	cursor := NewCursor(0, 0)
-	textWidth := 20
 	textHeight := 20
 	textSpacing := 1
 
@@ -51,22 +46,34 @@ func main() {
 		// fmt.Println(screenWidth, screenHeight)
 
 		if rl.IsKeyPressed(rl.KeyL) {
-			cursor.MoveRight(1, 10)
+			cursor.MoveRight(1, len(lines[cursor.line]))
 		}
 		if rl.IsKeyPressed(rl.KeyH) {
 			cursor.MoveLeft(1)
+		}
+		if rl.IsKeyPressed(rl.KeyJ) {
+			cursor.MoveDown(1, len(lines)-1)
+			cursor.col = min(cursor.col, len(lines[cursor.line])-1)
+		}
+		if rl.IsKeyPressed(rl.KeyK) {
+			cursor.MoveUp(1)
+			cursor.col = min(cursor.col, len(lines[cursor.line])-1)
 		}
 
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 		for i, l := range lines {
-			rl.DrawTextEx(font, l, rl.Vector2{X: 0, Y: float32(i * textHeight)}, float32(textHeight), float32(textSpacing), rl.Black)
+			rl.DrawTextEx(rl.GetFontDefault(), l, rl.Vector2{X: 0, Y: float32(i) * float32(textHeight)}, float32(textHeight), float32(textSpacing), rl.Black)
+			if cursor.line == i {
+				// stringWidth := rl.MeasureText(l[0:cursor.col], int32(textHeight))
+				stringSize := rl.MeasureTextEx(rl.GetFontDefault(), l[0:cursor.col], float32(textHeight), float32(textSpacing))
+				fmt.Println(stringSize)
+				cursorPos := rl.Vector2{X: stringSize.X, Y: float32(cursor.line * textHeight)}
+				cursorBottom := rl.Vector2{X: cursorPos.X, Y: cursorPos.Y + float32(textHeight)}
+				rl.DrawLineV(cursorPos, cursorBottom, rl.Red)
+			}
 		}
-
-		cursorPos := rl.Vector2{X: float32(cursor.col * textWidth), Y: float32(cursor.line * textHeight)}
-		cursorBottom := rl.Vector2{X: cursorPos.X, Y: cursorPos.Y + float32(textHeight)}
-		rl.DrawLineV(cursorPos, cursorBottom, rl.Red)
 
 		rl.EndDrawing()
 	}
